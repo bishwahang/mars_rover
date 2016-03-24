b3b32af55211179afef17b21e44bb010e4fecf42
Bishwa Hang Rai <bishwahang.kirat@gmail.com>
2016-03-25 00:20:32 +0100

    caches results and prints all at once

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -8,12 +8,18 @@ module MarsRover
     Map.max_x = max_x.to_i
     Map.max_y = max_y.to_i
     test_cases = gets.chomp.to_i
+    results = []
 
     test_cases.times do
       current_postion = gets.chomp.split
       instructions = gets.chomp
       rover = MarsRover::Rover.new(x: current_postion[0].to_i, y: current_postion[1].to_i, direction: current_postion[2], instructions: instructions)
-      puts rover.execute!
+      results << rover.execute!
     end
+    puts results.join("\n")
   end
+end
+
+if $0 == __FILE__
+  MarsRover.run
 end
