732335428802aa1b65dfc2f4a727aedc47e7e742
Bishwa Hang Rai <bishwahang.kirat@gmail.com>
2016-03-25 15:12:21 +0100

    added min_x and min_y

--- lib/mars_rover/map.rb
+++ lib/mars_rover/map.rb
@@ -2,8 +2,11 @@ module MarsRover
   module Map
     class << self
       attr_accessor :max_x, :max_y
+      def min_x;return 0;end
+      def min_y;return 0;end
+
       def check_invalid?(position)
-        (position.x > max_x || position.y > max_y || position.x < 0 || position.y < 0) ? true : false
+        (position.x > max_x || position.y > max_y || position.x < min_x || position.y < min_y) ? true : false
       end
     end
   end
